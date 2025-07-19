70c4d75bd4473209b934e79e22e869eaa3782676
liuge6668 <liuge6668@gmail.com>
2025-07-19 22:10:21 +0800

    feat: 启用cz

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-    'stylelint-config-prettier'
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-config-prettier'],
   plugins: ['stylelint-order', 'stylelint-prettier'],
   rules: {
     'selector-class-pattern': null
   }
-};
+}
